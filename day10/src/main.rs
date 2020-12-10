fdc226e52a47f13c8b391d0121679917ff126478
Conrad Ludgate <conradludgate@gmail.com>
2020-12-10 15:58:29 +0000

    day10 clean up

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,51 +1,8 @@
 mod parse;
 
-fn gaps(numbers: &[usize]) -> (usize, usize) {
-
-    let mut ones = 0;
-    let mut threes = 1;
-
-    if numbers[0] == 1 {
-        ones += 1;
-    } else if numbers[0] == 3 {
-        threes += 1;
-    }
-
-    for i in 1..numbers.len() {
-        let diff = numbers[i] - numbers[i - 1];
-        if diff == 1 {
-            ones += 1;
-        }
-        if diff == 3 {
-            threes += 1;
-        }
-    }
-
-    (ones, threes)
-}
-
-#[test]
-fn test_gaps_1() {
-    let mut numbers = vec![16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4];
+fn runs(mut numbers: Vec<usize>) -> Vec<usize> {
     numbers.sort_unstable();
-    let (ones, threes) = gaps(&numbers);
-    assert_eq!(ones, 7);
-    assert_eq!(threes, 5)
-}
 
-#[test]
-fn test_gaps_2() {
-    let mut numbers = vec![
-        28, 33, 18, 42, 31, 14, 46, 20, 48, 47, 24, 23, 49, 45, 19, 38, 39, 11, 1, 32, 25, 35, 8,
-        17, 7, 9, 4, 2, 34, 10, 3,
-    ];
-    numbers.sort_unstable();
-    let (ones, threes) = gaps(&numbers);
-    assert_eq!(ones, 22);
-    assert_eq!(threes, 10)
-}
-
-fn arrangements(numbers: &[usize]) -> usize {
     let mut runs = vec![];
 
     let mut run = 1;
@@ -62,37 +19,81 @@ fn arrangements(numbers: &[usize]) -> usize {
     }
     runs.push(run + 1);
 
-    let map = vec![0, 1, 1, 2, 4, 7];
-    runs.into_iter().map(|run| map[run]).fold(1, |acc, x| acc * x)
+    runs
+}
+
+#[test]
+fn test_runs_1() {
+    let numbers = vec![16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4];
+    let runs = runs(numbers);
+    assert_eq!(runs, vec![2, 4, 3, 2, 1]);
+}
+
+#[test]
+fn test_runs_2() {
+    let numbers = vec![
+        28, 33, 18, 42, 31, 14, 46, 20, 48, 47, 24, 23, 49, 45, 19, 38, 39, 11, 1, 32, 25, 35, 8,
+        17, 7, 9, 4, 2, 34, 10, 3,
+    ];
+    let runs = runs(numbers);
+    assert_eq!(runs, vec![5, 5, 1, 4, 3, 1, 5, 2, 1, 5]);
+}
+
+fn gaps(runs: &Vec<usize>) -> (usize, usize) {
+    let threes = runs.len();
+    let ones = runs.iter().sum::<usize>() - threes;
+    (ones, threes)
+}
+
+#[test]
+fn test_gaps_1() {
+    let numbers = vec![16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4];
+    let (ones, threes) = gaps(&runs(numbers));
+    assert_eq!(ones, 7);
+    assert_eq!(threes, 5)
+}
+
+#[test]
+fn test_gaps_2() {
+    let numbers = vec![
+        28, 33, 18, 42, 31, 14, 46, 20, 48, 47, 24, 23, 49, 45, 19, 38, 39, 11, 1, 32, 25, 35, 8,
+        17, 7, 9, 4, 2, 34, 10, 3,
+    ];
+    let (ones, threes) = gaps(&runs(numbers));
+    assert_eq!(ones, 22);
+    assert_eq!(threes, 10)
+}
+
+fn arrangements(runs: &Vec<usize>) -> usize {
+    let map = vec![0, 1, 1, 2, 4, 7]; // hard coded permutation map
+    runs.into_iter()
+        .map(|&run| map[run])
+        .fold(1, |acc, x| acc * x)
 }
 
 #[test]
 fn test_arrangements_1() {
-    let mut numbers = vec![16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4];
-    numbers.sort_unstable();
-
-    let arrangements = arrangements(&numbers);
+    let numbers = vec![16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4];
+    let arrangements = arrangements(&runs(numbers));
     assert_eq!(arrangements, 8);
 }
 
 #[test]
 fn test_arrangements_2() {
-    let mut numbers = vec![
+    let numbers = vec![
         28, 33, 18, 42, 31, 14, 46, 20, 48, 47, 24, 23, 49, 45, 19, 38, 39, 11, 1, 32, 25, 35, 8,
         17, 7, 9, 4, 2, 34, 10, 3,
     ];
-    numbers.sort_unstable();
-
-    let arrangements = arrangements(&numbers);
+    let arrangements = arrangements(&runs(numbers));
     assert_eq!(arrangements, 19208);
 }
 
 fn main() {
     let input = parse::read_file();
-    let (_, mut numbers) = parse::numbers(&input).unwrap();
-    numbers.sort_unstable();
+    let (_, numbers) = parse::numbers(&input).unwrap();
+    let runs = runs(numbers);
 
-    let (ones, threes) = gaps(&numbers);
+    let (ones, threes) = gaps(&runs);
     println!(
         "ones: {}, three: {}, answer: {}",
         ones,
@@ -100,6 +101,6 @@ fn main() {
         ones * threes
     );
 
-    let arrangements = arrangements(&numbers);
+    let arrangements = arrangements(&runs);
     println!("arrangements: {}", arrangements)
 }
